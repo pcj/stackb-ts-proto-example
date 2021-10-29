3ea06fe67a97eaf3c825c05baaa3f6c5751fe88a
Paul Johnston <paul.johnston@robinhood.com>
2021-10-29 17:59:47 -0600

    bazel build //app OK

---
+++ app/app.ts
@@ -0,0 +1,10 @@
+// import proto/thing.
+import { Thing } from 'unity/proto/thing/thing';
+// import { Place } from 'unity/proto/place/place';
+
+const thing: Thing = {
+    id: "foo",
+    // createdAt: undefined,
+};
+
+console.log(`Hello, ${thing}!`);
